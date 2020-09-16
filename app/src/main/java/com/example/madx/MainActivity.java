705f405fd92cfd406581d218d8422be9b0b27fe3
Dinuka Bashana <dinukabk@gmal.com>
2020-09-16 21:35:57 +0530

    initial

---
+++ app/src/main/java/com/example/madx/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.madx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
